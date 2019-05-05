19d88fb3a117c0e19f69c99a056184b14e374216
evillt <ijoec123@gmail.com>
2019-05-05 14:45:23 +0800

    feat: init

---
+++ docs/basic.md
@@ -0,0 +1,15 @@
+基本用法
+
+```vue
+<template>
+  <el-form-renderer/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+```
---
+++ src/el-form-renderer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="el-form-renderer">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ElFormRenderer',
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+.el-form-renderer {
+
+}
+</style>
